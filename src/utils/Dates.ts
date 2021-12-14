d541ab39eb10d6b82b96ad315e7230469fa4b1d2
BondToEarn Dev <dev@bondtoearn.com>
2021-12-14 13:30:17 +0700

    update

--- src/utils/Dates.ts
+++ src/utils/Dates.ts
@@ -2,7 +2,8 @@ import { BigInt, log } from "@graphprotocol/graph-ts";
 import { Token } from "../../generated/schema";
 
 export function dayFromTimestamp(timestamp: BigInt): string {
-    let day_ts = timestamp.toI32() - (timestamp.toI32() % 86400)
+    let day_ts = timestamp.toI32() - (timestamp.toI32() % 3600)
+    // let day_ts = timestamp.toI32() - (timestamp.toI32() % 86400)
     return day_ts.toString()
 }
 
